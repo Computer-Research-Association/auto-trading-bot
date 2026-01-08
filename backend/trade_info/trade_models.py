35b55ba9f4ac9c0bc58b13be32354e2cf3ea4991
Sungeun Park <sungeun050712@gmail.com>
2026-01-08 16:38:06 +0900

    feat: 거래 기본 저장용 스키마 추가

--- backend/trade_info/trade_models.py
+++ backend/trade_info/trade_models.py
@@ -6,3 +6,15 @@ from sqlmodel import SQLModel, Field, Index
 #거래 체결 시각알기 위함 (날짜 + 시간)
 def utcnow() -> datetime:
     return datetime.now(timezone.utc)
+
+#거래 1건을 DB 테이블 한 행으로 저장하기 위함
+class TradeModel(SQLModel, table=True):
+    #거래 기록 하나를 식별하는 번호 (DB가 자동으로 증가시킴)
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    market: str = Field(index= True) #어떤 코인 거래했는지
+    side: str = Field(index = True) # buy or sell
+    price: float
+    volume: float # 수량
+    fee: Optional[float] = Field(default=None # 거래시 발생한 수수료
+
