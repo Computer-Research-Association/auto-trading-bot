4db7078fdd4eed958c57a3bcb90db8feefaad831
dongdong7-max <dongdong7@handong.ac.kr>
2026-01-12 22:01:16 +0900

    feat: create indicators\trend.py get_ema

--- backend/trading/indicators/trend.py
+++ backend/trading/indicators/trend.py
@@ -8,3 +8,11 @@ def get_sma(df: pd.DataFrame, length: int = 20):
     결과: Series
     """
     return ta.sma(df['close'], length=length)
+
+
+def get_ema(df: pd.DataFrame, length: int = 20):
+    """
+    지수 이동평균선(EMA) 계산
+    """
+    return ta.ema(df['close'], length=length)
+    
