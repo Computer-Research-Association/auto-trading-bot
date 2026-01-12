c6ab9ab9a586ee16334f9961c0216a23a6ab3eca
dongdong7-max <dongdong7@handong.ac.kr>
2026-01-12 10:43:35 +0900

    fix: database.py(backend폴더 및) load_dotenv() 경로 수정

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,13 @@
 import os
 from dotenv import load_dotenv  
 from sqlmodel import create_engine, Session
+from pathlib import Path
 
 # 1. DB 접속 정보 설정 (이미지에서 설정한 정보와 일치해야 함)
 # 형식: postgresql://사용자:비밀번호@호스트:포트/DB이름
-load_dotenv()
+env_path = Path(__file__).resolve().parent.parent / ".env"
+load_dotenv(dotenv_path=env_path)
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 # 2. 엔진 생성
 engine = create_engine(DATABASE_URL, echo=True)  # echo=True는 SQL 로그를 터미널에 보여
