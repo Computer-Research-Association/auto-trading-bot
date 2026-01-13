4efd777d75c4d40e8dc137e99ae7cb5371436a38
이성호 <seongholee2003@gmail.com>
2026-01-13 17:26:53 +0900

    App.tsx 경로 수정 및 선언

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,17 @@
-// src/App.jsx
-import React from "react";
-import Header from "./components/Header";       // 고정된 헤더
-import Dashboard from "./features/pages/Dashboard";      // 메인 페이지
-import "./App.css"; // 전체 스타일
+import StrategyPanel from './features/Strategy/StrategyPanel';
+import Assets from './features/Assets/Assets';
+import History from './features/History/History';
+import OpenOrders from './features/OpenOrders/OpenOrders';
+import Dashboard from './features/pages/Dashboard'; 
 
-function App() {
+export default function App() {
   return (
-    <div className="app-root">
-      {/* 1. 헤더는 페이지가 바뀌어도 무조건 떠 있음 */}
-      <Header />
-
-      {/* 2. 그 아래에 현재 페이지를 보여줌 */}
-      <Dashboard />
-      
-      {/* 나중에 라우터를 쓰면 여기에 <Routes>...<Routes/>가 들어갑니다 */}
+    <div>
+      <StrategyPanel />
+      <Assets/>
+      <History/>
+      <OpenOrders/>
+      <Dashboard/>
     </div>
   );
-}
-
-export default App;
+};
