e1200dfe75f0a0a8c224e136f04998d9c4a1ab09
dongdong7-max <dongdong7@handong.ac.kr>
2026-01-16 14:52:34 +0900

    feat: create __init__ function

--- backend/trading/load_data.py
+++ backend/trading/load_data.py
@@ -6,5 +6,8 @@ from typing import Optional
 logger = logging.getLogger(__name__)
 
 class DataLoader:
+    def __init__(self, ticker: str = "KRW-BTC", interval: str = "minute15"):
+        self.ticker = ticker
+        self.interval = interval
 
-    
+    def fetch_ohlcv()
