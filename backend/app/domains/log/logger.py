ae2ee49d1e10d8ca7a6860c0e29586a03b95246e
dongdong7-max <dongdong7@handong.ac.kr>
2026-02-02 16:32:05 +0900

    feat: logger.py 시간 정보 표기 방법 수정

--- backend/app/domains/log/logger.py
+++ backend/app/domains/log/logger.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from datetime import datetime
+from datetime import datetime, timezone
 from enum import Enum
 from typing import Optional
 
@@ -51,7 +51,7 @@ async def log_to_db(
 ) -> OperatingLog:
 
     row = OperatingLog(
-        timestamp=timestamp or datetime.utcnow(),
+        timestamp=timestamp or datetime.now(timezone.utc),
         level=level.value,
         category=category.value,
         event_name=event_name,  # 필요하면 LogEvent로 강제 가능
@@ -86,3 +86,13 @@ async def log_event(
         timestamp=timestamp,
         commit=commit,
     )
+
+
+'''
+SELECT 
+    *,
+    created_at AT TIME ZONE 'Asia/Seoul' AS created_at_kst,
+    "timestamp" AT TIME ZONE 'Asia/Seoul' AS timestamp_kst,
+    updated_at AT TIME ZONE 'Asia/Seoul' AS updated_at_kst
+FROM operating_log order by timestamp desc limit 10;
+'''
